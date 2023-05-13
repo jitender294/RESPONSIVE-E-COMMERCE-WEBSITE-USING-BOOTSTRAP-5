633462188aca2a8e7f9308c0ee9014deed674b5e
Jitender2947 <124140694+Jitender2947@users.noreply.github.com>
2023-05-13 14:38:59 +0530

    Add untracked files

---
+++ index.js
@@ -0,0 +1,25 @@
+let dayItem = document.querySelector("#days");
+let hoursItem = document.querySelector("#hours");
+let minItem = document.querySelector("#min");
+let secItem = document.querySelector("#sec");
+
+
+let countDown = () => {
+    let futurDate = new Date(" 30 may 2022");
+    let currentDate = new Date();
+    let myDate = futurDate - currentDate;
+
+    let days = Math.floor(myDate / 1000 / 60 / 60 / 24);
+    let hours = Math.floor(myDate / 1000 / 60 / 60) % 24;
+    let min = Math.floor(myDate / 1000 / 60) % 60;
+    let sec = Math.floor(myDate / 1000) % 60;
+
+    dayItem.innerHTML = days;
+    hoursItem.innerHTML = hours;
+    minItem.innerHTML = min;
+    secItem.innerHTML = sec;
+}
+
+countDown()
+
+setInterval(countDown, 1000)
